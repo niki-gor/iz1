78a833dc64c14e0f32d6df1af405f7b63f71aadc
niki-gor <nic.gor@yandex.ru>
2022-03-18 09:44:18 +0000

    Add CMakeLists, gitignore and helloworld

---
+++ helloworldprint/helloworldprint.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "helloworldprint.h"
+
+void helloworldprint() {
+    printf("Hello, world!");
+}
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "helloworldprint.h"
+
+void main() {
+    helloworldprint();
+}
